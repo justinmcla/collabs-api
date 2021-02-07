404fdfb473c8dd4ddcac810d0f59714c4c763ac3
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-02-07 13:07:55 -0500

    adds profile tests to check for validity

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a user & name' do
+    profile = Profile.new(user: User.new, name: Faker::Name.name)
+    expect(profile).to be_valid
+  end
+  it 'is not valid without a user' do
+    profile = Profile.new(name: Faker::Name.name)
+    expect(profile).to_not be_valid
+  end
+  it 'is not valid without a name' do
+    profile = Profile.new(user: User.new)
+    expect(profile).to_not be_valid
+  end 
 end
