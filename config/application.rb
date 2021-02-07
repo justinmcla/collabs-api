c37e3f2977670cc35801f491951b0c659ea65adc
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-02-06 22:12:19 -0500

    fixes spelling of ActionDispatch::Cookies

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module CollabsApi
 
     # Enabling session cookies for the front-end browser client
     config.session_store :cookie_store, key: '_interslice_session'
-    config.middleware.use ActionDispatch::cookies
+    config.middleware.use ActionDispatch::Cookies
     config.middleware.use config.session_store, config.session_options
     
     # Enabling override of HTTP POST requests via method
