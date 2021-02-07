937fbfd48355cc5e93dc9ff5ba08995e674a6df5
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-02-07 13:07:40 -0500

    adds user tests to check for profile association

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,19 @@ RSpec.describe User, type: :model do
     user = User.new
     expect(user).to_not be_valid
   end
+  it 'has no profile to begin with' do
+    user = User.new(sub: 'some_id')
+    expect(user.profile).to eq nil
+  end
+  it 'can have 1 profile' do
+    user = User.new
+    user.build_profile(name: Faker::Name.name)
+    expect(user.profile).to_not eq nil
+  end
+  it 'can only have 1 profile' do
+    user = User.new 
+    user.build_profile(name: Faker::Name.name)
+    user.build_profile(name: Faker::Name.name)
+    expect(user.profile.class).to_not eq Array
+  end 
 end
