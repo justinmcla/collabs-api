5287ebb6d60e162ae16d2c883e01c0321ae14e2d
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-02-07 12:33:31 -0500

    adds test for user invalidity

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe User, type: :model do
     user = User.new(sub: 'some_id')
     expect(user).to be_valid
   end
+  it 'is not valid without a subscriber id' do
+    user = User.new
+    expect(user).to_not be_valid
+  end
 end
