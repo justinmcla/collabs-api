b58cb9b7676804fe8b3c353d5ab3a39f0317ccaf
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-02-07 12:36:48 -0500

    migrated profiles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_07_172114) do
+ActiveRecord::Schema.define(version: 2021_02_07_173453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.date "birthdate"
+    t.text "about"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "sub"
@@ -21,4 +31,5 @@ ActiveRecord::Schema.define(version: 2021_02_07_172114) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "profiles", "users"
 end
