f7cde451fdefeadaf080fe54f5e805f3386748b7
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-02-07 12:32:57 -0500

    adds test for user validity

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a subscriber id' do
+    user = User.new(sub: 'some_id')
+    expect(user).to be_valid
+  end
 end
