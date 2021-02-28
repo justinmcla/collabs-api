46b10f852d6332058cdf25052f61ebb4c19a2c06
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-02-28 10:48:26 -0500

    adds polymorphic assns to Profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,14 @@
 class Profile < ApplicationRecord
   belongs_to :user
 
+  # VALIDATIONS
   validates :name, presence: true
+
+  # POLYMORPHIC ASSOCIATIONS
+  has_many :locations, as: :locatable
+  has_many :jobs, as: :jobable
+  has_many :educations, as: :educatable
+  has_many :social_links, as: :linkable
+  has_many :languages, as: :languageable
+  
 end
