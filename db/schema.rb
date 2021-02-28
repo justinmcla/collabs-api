e49902de78a6562784584d6c3c3f1f37d5669b2e
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-02-28 10:48:36 -0500

    migrates db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_07_173453) do
+ActiveRecord::Schema.define(version: 2021_02_27_022543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "educations", force: :cascade do |t|
+    t.string "highest_level"
+    t.string "institution"
+    t.string "educatable_type", null: false
+    t.bigint "educatable_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["educatable_type", "educatable_id"], name: "index_educations_on_educatable_type_and_educatable_id"
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.string "employer"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string "jobable_type", null: false
+    t.bigint "jobable_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["jobable_type", "jobable_id"], name: "index_jobs_on_jobable_type_and_jobable_id"
+  end
+
+  create_table "languages", force: :cascade do |t|
+    t.string "lang"
+    t.string "proficiency"
+    t.string "languageable_type", null: false
+    t.bigint "languageable_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["languageable_type", "languageable_id"], name: "index_languages_on_languageable_type_and_languageable_id"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.string "locatable_type", null: false
+    t.bigint "locatable_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["locatable_type", "locatable_id"], name: "index_locations_on_locatable_type_and_locatable_id"
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -23,6 +66,25 @@ ActiveRecord::Schema.define(version: 2021_02_07_173453) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "social_links", force: :cascade do |t|
+    t.string "handle"
+    t.string "provider"
+    t.string "linkable_type", null: false
+    t.bigint "linkable_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["linkable_type", "linkable_id"], name: "index_social_links_on_linkable_type_and_linkable_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.string "taggable_type", null: false
+    t.bigint "taggable_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["taggable_type", "taggable_id"], name: "index_tags_on_taggable_type_and_taggable_id"
   end
 
   create_table "users", force: :cascade do |t|
