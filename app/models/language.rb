499588d13d6357b77c0b302e700fbd23a554000f
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-05 19:39:13 -0500

    Adds language validations

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,3 +1,10 @@
 class Language < ApplicationRecord
   belongs_to :languageable, polymorphic: true
+
+  validates :lang, presence: true
+  validates :proficiency, presence: true
+  validates :proficiency, inclusion: { 
+    in: %w(Beginner Advanced Fluent),
+    message: "%{value} is not a valid option"
+  }
 end
