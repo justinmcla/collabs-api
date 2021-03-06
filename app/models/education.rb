30ba1a1043952196bf9f24e27ed420149dda7a3a
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-05 19:38:18 -0500

    Adds validations

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,3 +1,16 @@
 class Education < ApplicationRecord
   belongs_to :educatable, polymorphic: true
+
+  validates :highest_level, presence: true
+  validates :highest_level, inclusion: { 
+    in: [
+      'High School',
+      'Some College', 
+      'Associate\'s Degree',
+      'Bachelor\'s Degree',
+      'Master\'s Degree',
+      'Doctoral Degree'
+    ], message: "%{value} is not a valid option"
+  }
+  validates :institution, presence: true
 end
