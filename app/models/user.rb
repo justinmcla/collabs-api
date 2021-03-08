6db05e878948af591b715a4661b7b515f53666c9
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-07 22:54:33 -0500

    adds uuid to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
+  before_validation :generate_uuid
+
   validates :sub, presence: true, uniqueness: true
+  validates :uuid, presence: true, uniqueness: true
 
   has_one :profile
+
+  private
+
+  def generate_uuid
+    self.uuid ||= SecureRandom.uuid
+  end
 end
