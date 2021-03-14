fc6b59c1145dbefefb8ae22e00b104264570ef98
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-14 13:04:10 -0400

    Adds user collaborations associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,15 @@ class User < ApplicationRecord
   validates :uuid, presence: true, uniqueness: true
 
   has_one :profile
+  has_many :collaborations_as_sender, class_name: "Collaboration", foreign_key: "sender_id"
+  has_many :collabs_as_sender, -> { where(collaborations: { accepted: true }) }, through: :collaborations_as_sender, source: :receiver
+  has_many :collaborations_as_receiver, class_name: "Collaboration", foreign_key: "receiver_id"
+  has_many :collabs_as_receiver, -> { where(collaborations: { accepted: true }) }, through: :collaborations_as_receiver, source: :sender
+
+  def collabs
+    ids = collabs_as_receiver.pluck(:id).concat(collabs_as_sender.pluck(:id))
+    User.where(id: ids)
+  end
 
   private
 
