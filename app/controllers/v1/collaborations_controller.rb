0af8b3d1ef68c4e07d02903a359165922d9babbf
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-18 09:20:44 -0400

    adds collaboration index action, revises show action

--- app/controllers/v1/collaborations_controller.rb
+++ app/controllers/v1/collaborations_controller.rb
@@ -1,7 +1,23 @@
 class V1::CollaborationsController < ApplicationController
+  def index
+    users = User.where("sub != '#{@sub}'").limit(5)
+    profiles = users.map { |user| user.profile }
+    render json: ProfileSerializer.new(profiles).serializable_hash[:data].map{ |profile| profile[:attributes] }
+  end
+
   def show
     user = User.find_by uuid: params[:id]
-    collabs = user.collabs.map{ |collab| collab.profile}
-    render json: { data: collabs, status: :ok }
+    user ? render_collabs(user) : render_not_found
+  end
+
+  private
+
+  def render_collabs(user)
+    profiles = user.collabs.map{ |collab| collab.profile }
+    render json: ProfileSerializer.new(profiles).serializable_hash[:data]
+  end
+
+  def render_not_found
+    render json: { status: :unprocessable_entity, errors: ['Record not found'] }
   end
 end
