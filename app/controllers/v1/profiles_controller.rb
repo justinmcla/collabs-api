1f29b2424c2faea6ea056b529a633e1ae2d6d7b7
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-18 09:21:11 -0400

    adds index, search action for profile, refactors show

--- app/controllers/v1/profiles_controller.rb
+++ app/controllers/v1/profiles_controller.rb
@@ -1,9 +1,31 @@
 class V1::ProfilesController < ApplicationController
+  def index
+    if params[:query]
+      profiles = Profile.where("name ILIKE ? OR profession ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
+      render json: ProfileSerializer.new(profiles).serializable_hash[:data].map{ |profile| profile[:attributes] }
+    end
+  end
+
   def show
     user = User.find_by uuid: params[:id]
-    render json: { data: user.profile, status: :ok }
+    user ? render_profile(user.profile) : render_not_found
   end
 
   def update
+  end
+
+  def search
+    profile = Profile.find_by(url: '/' + params[:endpoint])
+    profile ? render_profile(profile) : render_not_found
+  end
+
+  private
+
+  def render_profile(profile)
+    render json: ProfileSerializer.new(profile).serializable_hash[:data][:attributes]
+  end
+
+  def render_not_found
+    render json: { status: :unprocessable_entity, errors: ['Record not found'] }
   end
 end
