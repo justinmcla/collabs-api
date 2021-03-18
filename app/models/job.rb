c8078be191806b4a3907dff91a3c524a5cbbf52d
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-18 09:17:36 -0400

    deletes obsolete models

--- app/models/job.rb
+++
@@ -1,8 +0,0 @@
-class Job < ApplicationRecord
-  belongs_to :jobable, polymorphic: true
-
-  validates :title, presence: true
-  validates :employer, presence: true
-
-  has_one :location, as: :locatable
-end
