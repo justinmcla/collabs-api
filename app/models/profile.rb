0b87d3f32251919d9c5f413c7ab20bb51386f866
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-19 19:10:17 -0400

    adds profile_picture to profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,7 @@ class Profile < ApplicationRecord
   belongs_to :user
   has_one :location
   has_many :social_links
+  has_one_attached :profile_picture
 
   # VALIDATIONS
   validates :name, presence: true
