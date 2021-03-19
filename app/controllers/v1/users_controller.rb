f64b42596573986593a31b77637bdfec4484b8af
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-19 19:10:58 -0400

    rebuilds users controller to handle requests from collab search

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,11 +1,27 @@
 class V1::UsersController < ApplicationController
   def index
-    users = User.all
-    render json: users
+    if params['query'] == 'all'
+      profiles = User.all.map{ |user| user.profile }
+      render json: { data: ProfileSerializer.new(profiles).serializable_hash[:data].map{ |profile| profile[:attributes] }, status: 200 }
+    else
+      profiles = Profile.where("name ILIKE ? OR profession ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%").limit(5)
+      render json: { data: ProfileSerializer.new(profiles).serializable_hash[:data].map{ |profile| profile[:attributes] }, status: 200 }
+    end
   end
 
   def show
     user = User.find_by uuid: params[:id]
-    render json: user
+    user ? render_profile(user.profile) : render_not_found
   end
+
+  private
+
+  def render_profile(profile)
+    render json: ProfileSerializer.new(profile).serializable_hash[:data][:attributes]
+  end
+
+  def render_not_found
+    render json: { status: :unprocessable_entity, errors: ['Record not found'] }
+  end
+
 end
