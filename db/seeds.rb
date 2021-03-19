f39bad8b0d206c07e5de77feedd8d6cb10d5a72f
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-19 19:09:20 -0400

    adds post seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
 
 # Creates n users with randomly generated subscriber identities
 10.times { User.create(sub: "seed_data | #{SecureRandom.uuid}") }
@@ -32,4 +31,13 @@ User.all.each do |user|
   social_data.each do |data|
     user.profile.social_links.create(data)
   end
+end
+
+User.all.each do |user|
+  post_data = {
+    title: Faker::Lorem.sentence,
+    content: Faker::Lorem.paragraphs.join(' '),
+    author: user
+  }
+  Post.create(post_data)
 end
