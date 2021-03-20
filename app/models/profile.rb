80afe9d57aa7bb20e419e06dd69d706c38558ae7
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-20 09:13:53 -0400

    adds image_url method for profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -7,4 +7,8 @@ class Profile < ApplicationRecord
   # VALIDATIONS
   validates :name, presence: true
   validates :url, presence: true, uniqueness: true
+
+  def image_url
+    Rails.application.routes.url_helpers.url_for(profile_picture) if profile_picture.attached?
+  end
 end
