a5acca69860ba9c17f716e80026dbfa20a8fdc67
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-20 09:13:31 -0400

    add image_url to serializer

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,6 +1,6 @@
 class ProfileSerializer
   include JSONAPI::Serializer
-  attributes :id, :name, :birthdate, :about, :url, :profession, :location, :social_links
+  attributes :id, :name, :birthdate, :about, :url, :profession, :location, :social_links, :image_url
   attribute :uuid do |object|
     "#{object.user.uuid}"
   end
