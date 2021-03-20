f729b961707412950d0c5e3abd32393193fc3956
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-20 09:15:15 -0400

    adds strong_params, create & updates index to handle queries

--- app/controllers/v1/posts_controller.rb
+++ app/controllers/v1/posts_controller.rb
@@ -1,8 +1,28 @@
 class V1::PostsController < ApplicationController
   def index
-    render json: { data: PostSerializer.new(Post.all).serializable_hash[:data].map{ |post| post[:attributes] }, status: 200 }
+    if params['query'] == 'all'
+      posts = Post.all
+      render json: { data: PostSerializer.new(posts).serializable_hash[:data].map{ |post| post[:attributes] }, status: 200 }
+    else
+      posts = Post.where("title ILIKE ? OR content ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
+      render json: { data: PostSerializer.new(posts).serializable_hash[:data].map{ |post| post[:attributes] }, status: 200 }
+    end
   end 
 
   def create
+    user = User.find_by sub: @sub
+    post = Post.new(strong_params)
+    post.author = user
+    if post.save
+      render json: { data: PostSerializer.new(post).serializable_hash[:data][:attributes], status: 200 }
+    else
+      render json: { errors: ['Unable to create post'], status: 422 }
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:post).permit(:id, :title, :content)
   end
 end
