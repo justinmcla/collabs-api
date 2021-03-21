8aade7d6aaa24a56db905c4255f26e23cf0a35c6
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-21 15:08:45 -0400

    specify routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :v1 do
     resources :sessions
-    resources :profiles
-    resources :collaborations
-    resources :collaboration_requests
+    
+    get '/profiles', to: 'profiles#index'
+    patch '/profiles/:id', to: 'profiles#update'
+
+    get '/collaborations', to: 'collaborations#index'
+    post '/collaborations', to: 'collaborations#create'
+    patch '/collaborations/:id', to: 'collaborations#update'
+    delete '/collaborations/:id', to: 'collaborations#destroy'
+    get '/collaborations/pending', to: 'collaborations#pending'
+    
     resources :posts
     resources :users
     get '/profiles/search/:endpoint', to: 'profiles#search'
