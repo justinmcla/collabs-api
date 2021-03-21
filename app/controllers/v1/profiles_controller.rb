c0248ed7988e71269ebf7b9d7fb7bce7e2512f07
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-21 15:08:24 -0400

    restructure how data is rendered in profiles controller

--- app/controllers/v1/profiles_controller.rb
+++ app/controllers/v1/profiles_controller.rb
@@ -1,42 +1,59 @@
 class V1::ProfilesController < ApplicationController
   def index
-    if params[:query] == 'all'
-      users = User.where("sub != '#{@sub}'").limit(5)
-      profiles = users.map { |user| user.profile unless user.collabs.include(User.find_by(sub: @sub)) }  
-      render json: ProfileSerializer.new(profiles).serializable_hash[:data].map{ |profile| profile[:attributes] }
+    if params['query'] == 'all'
+      profiles = Profile.where.not(user: @user).limit(20)
+      render json: {
+        data: ProfileSerializer.new(profiles).serializable_hash[:data].map{ |profile| profile[:attributes] }, 
+        status: 200 
+      }
     else
-      profiles = Profile.where("name ILIKE ? OR profession ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%").limit(5)
-      render json: ProfileSerializer.new(profiles).serializable_hash[:data].map{ |profile| profile[:attributes] }
+      profiles = Profile.where.not(user: @user).where("name ILIKE ? OR profession ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%").limit(20)
+      render json: { 
+        data: ProfileSerializer.new(profiles).serializable_hash[:data].map{ |profile| profile[:attributes] }, 
+        status: 200 
+      }
     end
   end
 
-  def show
-    user = User.find_by uuid: params[:id]
-    user ? render_profile(user.profile) : render_not_found
-  end
-
   def update
-    user = User.find_by sub: @sub
-    user.profile.update(strong_params)
-    render json: { data: ProfileSerializer.new(user.profile).serializable_hash[:data][:attributes], status: 200 }
+    profile = @user.profile
+    profile.assign_attributes(strong_params)
+    if profile.save 
+      render json: { 
+        data: ProfileSerializer.new(profile).serializable_hash[:data][:attributes], 
+        status: 200 
+      }
+    else
+      render json: {
+        errors: profile.errors,
+        status: 422
+      }
+    end 
   end
 
   def search
-    profile = Profile.find_by(url: '/' + params[:endpoint])
-    profile ? render_profile(profile) : render_not_found
+    profile = Profile.find_by(url: params[:endpoint])
+    if profile 
+      render json: { 
+        data: ProfileSerializer.new(profile).serializable_hash[:data][:attributes],
+        status: 200
+      }
+    else
+      render json: {
+        errors: ['Record not found'],
+        status: 422
+      }
+    end
   end
 
   private
 
-  def render_profile(profile)
-    render json: ProfileSerializer.new(profile).serializable_hash[:data][:attributes]
-  end
-
-  def render_not_found
-    render json: { status: :unprocessable_entity, errors: ['Record not found'] }
-  end
-
   def strong_params
-    params.require(:profile).permit(:id, :name, :birthdate, :profile_picture)
+    params.require(:profile).permit(
+      :id, :name, :birthdate, :profession, :about, :url, 
+      :city, :state, :zip, :profile_picture,
+      :facebook, :github, :instagram, :linkedin, :medium, 
+      :twitter, :youtube, :twitch
+    )
   end
 end
