0d97bd12bc88ac1f0a772f3b0b2d7e74d4e8c463
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-21 15:07:30 -0400

    revise how users are rendered

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,27 +1,21 @@
 class V1::UsersController < ApplicationController
   def index
     if params['query'] == 'all'
-      profiles = User.all.map{ |user| user.profile }
-      render json: { data: ProfileSerializer.new(profiles).serializable_hash[:data].map{ |profile| profile[:attributes] }, status: 200 }
+      profiles = Profile.where.not(user: @user).limit(20)
+      render json: {
+        data: ProfileSerializer.new(profiles).serializable_hash[:data].map{ |profile| profile[:attributes] }, 
+        status: 200 }
     else
-      profiles = Profile.where("name ILIKE ? OR profession ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%").limit(5)
+      profiles = Profile.where.not(user: user).where("name ILIKE ? OR profession ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%").limit(20)
       render json: { data: ProfileSerializer.new(profiles).serializable_hash[:data].map{ |profile| profile[:attributes] }, status: 200 }
     end
   end
 
   def show
     user = User.find_by uuid: params[:id]
-    user ? render_profile(user.profile) : render_not_found
+    render json: { 
+      data: ProfileSerializer.new(user.profile).serializable_hash[:data][:attributes], 
+      status: 200 
+    }
   end
-
-  private
-
-  def render_profile(profile)
-    render json: ProfileSerializer.new(profile).serializable_hash[:data][:attributes]
-  end
-
-  def render_not_found
-    render json: { status: :unprocessable_entity, errors: ['Record not found'] }
-  end
-
 end
