96d59acded9d8de4f71ea938fae3d73f4ca610a2
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-21 12:27:49 -0400

    adds attributes, fixes uuid

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,7 +1,10 @@
 class ProfileSerializer
   include JSONAPI::Serializer
-  attributes :id, :name, :birthdate, :about, :url, :profession, :location, :social_links, :image_url
+  attributes :id, :user_id, :name, :birthdate, :about, :url, :profession, 
+    :image_url, :city, :state, :zip, :facebook, :twitter, :instagram, 
+    :github, :linkedin, :medium, :twitter, :youtube, :twitch
+  
   attribute :uuid do |object|
-    "#{object.user.uuid}"
+    object.user.uuid
   end
 end
