c99e8eaa32450fbe161ad805204a1c42b16676ee
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-21 12:25:01 -0400

    set user before all actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::API
   include Secured
+  before_action :set_current_user
+  
+  private
+
+  def set_current_user
+    @user = User.find_by sub: @sub
+  end
 end
